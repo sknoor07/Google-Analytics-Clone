273459940fb11ec3ddee0918ab645729b24c4b50
sknoor07 <114433576+sknoor07@users.noreply.github.com>
2026-07-14 12:24:06 +0530

    Add new API routes to public route matcher

--- middleware.tsx
+++ middleware.tsx
@@ -4,7 +4,9 @@ const isPublicRoute = createRouteMatcher([
     '/sign-in(.*)',
     '/sign-up(.*)',
     '/',
-    '/analytics.js'
+    '/analytics.js',
+    '/api/track(.*)',
+    '/api/live-user(.*)',
 ])
 
 export default clerkMiddleware(async (auth, req) => {
